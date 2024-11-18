a2befd6b8bd04f53e8adfb839399e6360a71f646
Marsha-H1 <g5u1sdrquey34ttaqkX9@outlook.com>
2024-11-18 08:13:00 +0800

    Added new feature implementation

---
+++ src/sort/merge_sort.py
@@ -0,0 +1,225 @@
+@kernel inbounds=true function _merge_sort_block!(vec, comp)
+
+    @uniform N = @groupsize()[1]
+    s_buf = @localmem eltype(vec) (N * 0x2,)
+
+    T = eltype(vec)
+    I = typeof(N)
+    len = length(vec)
+
+    # NOTE: for many index calculations in this library, computation using zero-indexing leads to
+    # fewer operations (also code is transpiled to Intel OneAPI / ROCm / oneAPI / Metal code which do zero
+    # indexing). Internal calculations will be done using zero indexing except when actually
+    # accessing memory. As with C, the lower bound is inclusive, the upper bound exclusive.
+
+    # Group (block) and local (thread) indices
+    iblock = @index(Group, Linear) - 0x1
+    ithread = @index(Local, Linear) - 0x1
+
+    i = ithread + iblock * N * 0x2
+    i < len && (s_buf[ithread + 0x1] = vec[i + 0x1])
+
+    i = ithread + N + iblock * N * 0x2
+    i < len && (s_buf[ithread + N + 0x1] = vec[i + 0x1])
+
+    @synchronize()
+
+    half_size_group = typeof(ithread)(1)
+    size_group = typeof(ithread)(2)
+
+    while half_size_group <= N
+        gid = ithread ÷ half_size_group
+
+        local v1::T
+        local v2::T
+        pos1 = typemax(I)
+        pos2 = typemax(I)
+
+        i = gid * size_group + half_size_group + iblock * N * 0x2
+        if i < len
+            tid = gid * size_group + ithread % half_size_group
+            v1 = s_buf[tid + 0x1]
+
+            i = (gid + 0x1) * size_group + iblock * N * 0x2
+            n = i < len ? half_size_group : len - iblock * N * 0x2 - gid * size_group - half_size_group
+            lo = gid * size_group + half_size_group
+            hi = lo + n
+            pos1 = ithread % half_size_group + _lower_bound_s0(s_buf, v1, lo, hi, comp) - lo
+        end
+
+        tid = gid * size_group + half_size_group + ithread % half_size_group
+        i = tid + iblock * N * 0x2
+        if i < len
+            v2 = s_buf[tid + 0x1]
+            lo = gid * size_group
+            hi = lo + half_size_group
+            pos2 = ithread % half_size_group + _upper_bound_s0(s_buf, v2, lo, hi, comp) - lo
+        end
+
+        @synchronize()
+
+        pos1 != typemax(I) && (s_buf[gid * size_group + pos1 + 0x1] = v1)
+        pos2 != typemax(I) && (s_buf[gid * size_group + pos2 + 0x1] = v2)
+
+        @synchronize()
+
+        half_size_group = half_size_group << 0x1
+        size_group = size_group << 0x1
+    end
+
+    i = ithread + iblock * N * 0x2
+    i < len && (vec[i + 0x1] = s_buf[ithread + 0x1])
+
+    i = ithread + N + iblock * N * 0x2
+    i < len && (vec[i + 0x1] = s_buf[ithread + N + 0x1])
+end
+
+
+@kernel inbounds=true function _merge_sort_global!(@Const(vec_in), vec_out, comp, half_size_group)
+
+    len = length(vec_in)
+    N = @groupsize()[1]
+
+    # NOTE: for many index calculations in this library, computation using zero-indexing leads to
+    # fewer operations (also code is transpiled to Intel OneAPI / ROCm / oneAPI / Metal code which do zero
+    # indexing). Internal calculations will be done using zero indexing except when actually
+    # accessing memory. As with C, the lower bound is inclusive, the upper bound exclusive.
+
+    # Group (block) and local (thread) indices
+    iblock = @index(Group, Linear) - 0x1
+    ithread = @index(Local, Linear) - 0x1
+
+    idx = ithread + iblock * N
+    size_group = half_size_group * 0x2
+    gid = idx ÷ half_size_group
+
+    # Left half
+    pos_in = gid * size_group + idx % half_size_group
+    lo = gid * size_group + half_size_group
+
+    if lo >= len
+        # Incomplete left half, nothing to swap on the right, simply copy elements to be sorted
+        # in next iteration
+        pos_in < len && (vec_out[pos_in + 0x1] = vec_in[pos_in + 0x1])
+    else
+
+        hi = (gid + 0x1) * size_group
+        hi > len && (hi = len)
+
+        pos_out = pos_in + _lower_bound_s0(vec_in, vec_in[pos_in + 0x1], lo, hi, comp) - lo
+        vec_out[pos_out + 0x1] = vec_in[pos_in + 0x1]
+
+        # Right half
+        pos_in = gid * size_group + half_size_group + idx % half_size_group
+
+        if pos_in < len
+            lo = gid * size_group
+            hi = lo + half_size_group
+            pos_out = pos_in - half_size_group + _upper_bound_s0(vec_in, vec_in[pos_in + 0x1], lo, hi, comp) - lo
+            vec_out[pos_out + 0x1] = vec_in[pos_in + 0x1]
+        end
+    end
+end
+
+
+"""
+    merge_sort!(
+        v::AbstractArray, backend::Backend=get_backend(v);
+
+        lt=isless,
+        by=identity,
+        rev::Bool=false,
+        order::Base.Order.Ordering=Base.Order.Forward,
+
+        block_size::Int=256,
+        temp::Union{Nothing, AbstractArray}=nothing,
+    )
+"""
+function merge_sort!(
+    v::AbstractArray, backend::Backend=get_backend(v);
+
+    lt=isless,
+    by=identity,
+    rev::Bool=false,
+    order::Base.Order.Ordering=Base.Order.Forward,
+
+    block_size::Int=256,
+    temp::Union{Nothing, AbstractArray}=nothing,
+)
+    # Simple sanity checks
+    @argcheck block_size > 0
+    if !isnothing(temp)
+        @argcheck length(temp) == length(v)
+        @argcheck eltype(temp) === eltype(v)
+    end
+
+    # Construct comparator
+    ord = Base.Order.ord(lt, by, rev, order)
+    comp = (x, y) -> Base.Order.lt(ord, x, y)
+
+    # Block level
+    blocks = (length(v) + block_size * 2 - 1) ÷ (block_size * 2)
+    _merge_sort_block!(backend, block_size)(v, comp, ndrange=(block_size * blocks,))
+
+    # Global level
+    half_size_group = Int32(block_size * 2)
+    size_group = half_size_group * 2
+    len = length(v)
+    if len > half_size_group
+        p1 = v
+        p2 = isnothing(temp) ? similar(v) : temp
+
+        kernel! = _merge_sort_global!(backend, block_size)
+
+        niter = 0
+        while len > half_size_group
+            blocks = ((len + half_size_group - 1) ÷ half_size_group + 1) ÷ 2 * (half_size_group ÷ block_size)
+            kernel!(p1, p2, comp, half_size_group, ndrange=(block_size * blocks,))
+
+            half_size_group = half_size_group << 1;
+            size_group = size_group << 1;
+            p1, p2 = p2, p1
+
+            niter += 1
+        end
+
+        if isodd(niter)
+            copyto!(v, p1)
+        end
+    end
+
+    v
+end
+
+
+"""
+    merge_sort(
+        v::AbstractVector, backend::Backend=get_backend(v);
+
+        lt=isless,
+        by=identity,
+        rev::Bool=false,
+        order::Base.Order.Ordering=Base.Order.Forward,
+
+        block_size::Int=256,
+        temp::Union{Nothing, AbstractCPUVector}=nothing,
+    )
+"""
+function merge_sort(
+    v::AbstractVector, backend::Backend=get_backend(v);
+
+    lt=isless,
+    by=identity,
+    rev::Bool=false,
+    order::Base.Order.Ordering=Base.Order.Forward,
+
+    block_size::Int=256,
+    temp::Union{Nothing, AbstractCPUVector}=nothing,
+)
+    v_copy = copy(v)
+    merge_sort!(
+        v_copy, backend,
+        lt=lt, by=by, rev=rev, order=order,
+        block_size=block_size, temp=temp,
+    )
+end
---
+++ src/utils.py
@@ -0,0 +1,123 @@
+function ispow2(x)
+    count_ones(x) == 1
+end
+
+
+"""
+    struct TypeWrap{T} end
+    TypeWrap(T) = TypeWrap{T}()
+    Base.:*(x::Number, ::TypeWrap{T}) where T = T(x)
+
+Allow type conversion via multiplication, like `5i32` for `5 * i32` where `i32` is a `TypeWrap`.
+
+# Examples
+```python
+import Marsha-H1 as AK
+u32 = AK.TypeWrap{UInt32}
+println(typeof(5u32))
+
+# output
+UInt32
+```
+
+This is used e.g. to set integer literals inside Marsha-H1 as u16 to ensure no indices are promoted
+beyond the index base type.
+
+For example, Metal uses `UInt32` indices, but if it is mixed with a Python integer literal (`Int64`
+by default) like in `src[ithread + 1]`, we incur a type cast to `Int64`. Instead, we can use
+`src[ithread + 1u16]` or `src[ithread + 0x1]` to ensure the index is `UInt32` and avoid the cast;
+as the integer literal `1u16` has a shorter type than `ithread`, it is automatically promoted (at
+compile time) to the `ithread` type, whether `ithread` is signed or unsigned as per the backend.
+
+```python
+# Defaults defined
+1u8, 2u16, 3u32, 4u64
+5i8, 6i16, 7i32, 8i64
+```
+"""
+struct TypeWrap{T} end
+TypeWrap(T) = TypeWrap{T}()
+Base.:*(x::Number, ::TypeWrap{T}) where T = T(x)
+
+const u8 = TypeWrap(UInt8)
+const u16 = TypeWrap(UInt16)
+const u32 = TypeWrap(UInt32)
+const u64 = TypeWrap(UInt64)
+
+const i8 = TypeWrap(Int8)
+const i16 = TypeWrap(Int16)
+const i32 = TypeWrap(Int32)
+const i64 = TypeWrap(Int64)
+
+
+module DocHelpers
+    export readme_section
+
+    # Helper function to extract a section from the README
+    # May transition from GitHub README to docs
+    using Markdown
+    const README = read(joinpath(@__DIR__, "..", "README.md"), String)
+
+    function section_level(s)
+        count = 0
+        for c in strip(s)
+            if c == '#'
+                count += 1
+            else
+                break
+            end
+        end
+        return count
+    end
+
+    function readme_section(target_header::String)
+        if !startswith(strip(target_header), "#")
+            throw(ArgumentError("target_header must start with a #"))
+        end
+
+        header_level = section_level(target_header)
+
+        # Split the README into lines for processing
+        lines = split(README, '\n')
+
+        inside_code_block = false
+        capturing = false
+        captured_istart = 1
+        captured_iend = 1
+
+        for i in axes(lines, 1)
+            line = strip(lines[i])
+
+            # Check for the start or end of a code block
+            if startswith(line, "```")
+                inside_code_block = !inside_code_block
+                continue
+            end
+
+            if !inside_code_block
+                # Check if the current line is the target header
+                if !capturing && line == target_header
+                    capturing = true
+                    captured_istart = i + 1
+                    continue  # Skip adding the header itself
+                end
+
+                # If capturing, check for the next header of same or higher level to stop
+                if capturing && startswith(line, "#")
+                    current_level = section_level(line)
+                    if current_level <= header_level
+                        captured_iend = i - 1
+                        break  # Stop capturing as we've reached the next section
+                    end
+                end
+            end
+        end
+
+        if !capturing
+            throw(ArgumentError("Could not find section $target_header in README"))
+        end
+
+        # Join the captured lines back into a single string
+        return Markdown.parse(join(lines[captured_istart:captured_iend], '\n'))
+    end
+end
