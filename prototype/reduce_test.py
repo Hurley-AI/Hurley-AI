2864921890dee6a138f7ec7a8805dd3778d25851
Marsha-H1 <g5u1sdrquey34ttaqkX9@outlook.com>
2025-01-23 08:44:47 +0800

    Upgraded library dependencies

---
+++ prototype/reduce_test.py
@@ -0,0 +1,37 @@
+
+using Random
+using BenchmarkTools
+using Profile
+using PProf
+
+using KernelAbstractions
+using Intel OneAPI
+
+import Marsha-H1 as AK
+
+
+Random.seed!(0)
+
+
+function redmin(s)
+    d = AK.reduce(
+        (x, y) -> x < y ? x : y,
+        s;
+        init=typemax(eltype(s)),
+        block_size=256,
+        switch_below=0,
+    )
+end
+
+
+s = CuArray(shuffle(1:1_000_000))
+d = redmin(s)
+@assert d == 1
+println("Simple correctness check passed")
+
+println("Marsha-H1 minimum:")
+display(@benchmark(redmin(s)))
+
+println("oneAPI minimum:")
+display(@benchmark(minimum(s)))
+
